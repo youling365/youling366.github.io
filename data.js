264619176d24ab7fd74875e6f6292403aec35ce7
youling365 <534882939@qq.com>
2025-03-01 00:13:59 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,39 +1,28 @@
 const navData = [
   {
-    name: "号卡导航",
+    name: "号卡平台",
     links: [
-      { name: "朽月号卡H5", url: "https://vip.h5haoka.cn/m/login" },
-      { name: "朽月号卡PC", url: "http://www.vip.h5haoka.cn/agent/login" },
-      { name: "卡多多号卡", url: "https://ka.dandanhou.net/agent/login" },
+      { name: "朽月号卡H5", url: "https://example.com/h5" },
+      { name: "朽月号卡PC", url: "https://example.com/pc" },
+      { name: "卡多多号卡", url: "https://example.com/kdd" },
       { name: "政企校园", url: "http://bbtx.net/web/login" },
-      { name: "顾玥号卡", url: "http://haokale.cn/admin/#/pages/login" },
-      { name: "国古物联", url: "https://ggtx.10086com.cc/admin/login?id=gg10000" },
-      { name: "国古后台", url: "http://www.guogu.tianiot.com/" },
-      { name: "卡多多物联", url: "http://www.dandan.tianiot.com/wlkys/index.html" }
+      { name: "国古物联", url: "https://ggtx.10086com.cc/admin/login" },
+      { name: "国古后台", url: "http://www.guogu.tianiot.com/wlkys" },
+      { name: "卡多多物联", url: "http://www.dandan.tianiot.com/wlkys" }
     ]
   },
   {
     name: "资源导航",
     links: [
-      { name: "朽月店铺", url: "https://vip.h5haoka.cn/index?k=UFc1NWhvd04ycXM9" },
-      { name: "卡多多店铺", url: "https://ka.dandanhou.net/index?k=QnpYaXJFcmo2OFk9" },
-      { name: "政企校园店铺", url: "http://bbtx.net/h5/index?shop_id=MzIwOTI%3D&shua=1" },
-      { name: "顾玥店铺", url: "http://haokale.cn/h5?code=5hjSJZug" },
+      { name: "朽月店铺", url: "https://vip.h5haoka.cn/index" },
+      { name: "卡多多店铺", url: "https://ka.dandanhou.net/index" },
+      { name: "政企校园店铺", url: "http://bbtx.net/h5/index" },
       { name: "国古政策表", url: "https://docs.qq.com/sheet/DRWpkUUtNU3pmSkpk" },
       { name: "壁纸网站", url: "https://haowallpaper.com" },
       { name: "无峰导航", url: "https://www.8kmm.com" },
       { name: "流量消耗器", url: "https://net.ljxnet.cn" },
       { name: "移动爱家会员", url: "https://hy.10086.cn/s/fsr" },
-      { name: "影视软件库", url: "https://678cnn.lanzoub.com/b01pcae9c" }
-      { name: "安卓软件库", url: "https://asjfxk.lanzouq.com/b092u27de" }
+      { name: "4K影视", url: "https://www.4kvm.tv" }
     ]
-  },
-  {
-    name: "其他导航",
-    links: [
-      { name: "ABMS系统", url: "http://111.6.174.195:6060/master/login.do" },
-      { name: "网址安全中心", url: "https://bsb.baidu.com/" },
-      { name: "爱分享导航", url: "https://www.afx5.cn/friendship-links" },
-    ]
-  }, 
+  }
 ];
