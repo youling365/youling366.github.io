20c6254a5b81760fa82d686d717d528e5a36cf01
youling365 <534882939@qq.com>
2025-02-28 22:15:09 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,28 @@
+const navData = [
+  {
+    name: "号卡平台",
+    links: [
+      { name: "朽月号卡H5", url: "https://example.com/h5" },
+      { name: "朽月号卡PC", url: "https://example.com/pc" },
+      { name: "卡多多号卡", url: "https://example.com/kdd" },
+      { name: "政企校园", url: "http://bbtx.net/web/login" },
+      { name: "国古物联", url: "https://ggtx.10086com.cc/admin/login" },
+      { name: "国古后台", url: "http://www.guogu.tianiot.com/wlkys" },
+      { name: "卡多多物联", url: "http://www.dandan.tianiot.com/wlkys" }
+    ]
+  },
+  {
+    name: "资源平台",
+    links: [
+      { name: "朽月店铺", url: "https://vip.h5haoka.cn/index" },
+      { name: "卡多多店铺", url: "https://ka.dandanhou.net/index" },
+      { name: "政企校园店铺", url: "http://bbtx.net/h5/index" },
+      { name: "国古政策表", url: "https://docs.qq.com/sheet/DRWpkUUtNU3pmSkpk" },
+      { name: "壁纸网站", url: "https://haowallpaper.com" },
+      { name: "无峰导航", url: "https://www.8kmm.com" },
+      { name: "流量消耗器", url: "https://net.ljxnet.cn" },
+      { name: "移动爱家会员", url: "https://hy.10086.cn/s/fsr" },
+      { name: "4K影视", url: "https://www.4kvm.tv" }
+    ]
+  }
+];
